faa817a40593a47052fd91f62af3d744406382ac
Haiyala <mathewhaiyala@gmail.com>
2023-08-11 19:42:55 -0700

    Hi

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,22 +6,20 @@
 
 int main(void)
 {
-        char c;
-
+	char c;
 	char d;
 
 	c = 'a';
 	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'Z') {
-			putchar(d);
-			d++;
-		}
+
+	while (c <= 'z') {
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z') {
+		putchar(d);
+		d++;
+	}
 	putchar('\n');
 	return (0);
 }
