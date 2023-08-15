c99c17cf505a4bf025a446229d9517be09171674
Haiyala <mathewhaiyala@gmail.com>
2023-08-15 05:50:52 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,19 +7,18 @@
 
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
 
-	i = 0;
-	while (i < 10)
-	{
-		ch = 'a';
-		while (ch <='z')
-		{
-			-putchar(ch);
-			ch++;
-		}
-		-putchar('\n');
-		i++;
-	}
+char ch;
+int i;
+i = 0;
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
+
 }
