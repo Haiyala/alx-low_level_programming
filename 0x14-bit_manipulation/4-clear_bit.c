936b4d4e089caf1a9a1444211d1626e76dd5f97a
Haiyala <mathewhaiyala@gmail.com>
2023-09-28 05:18:45 -0700

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,18 +2,16 @@
 
 /**
  * clear_bit - Clears a bit at a specified index.
- * @n: Pointer to an unsigned long int.
- * @index: Index of the bit to clear.
- * Returns: 1 on success, -1 on failure.
+ * @n: Input number.
+ * @index: Index to clear.
+ *
+ * Return: Always 0.
  */
-
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max = ~(max << index);
-	if (max == 0x00)
+	if (index > 63)
 		return (-1);
-	*n &= max;
+
+	*n = *n & ~(1ul << index);
 	return (1);
 }
