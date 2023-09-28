92779bd6099286785485309cb5bf4b2a5955a1d6
Haiyala <mathewhaiyala@gmail.com>
2023-09-28 04:04:26 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,25 +2,25 @@
 
 /**
  * binary_to_uint - Convert binary string to unsigned integer.
- * @y: Pointer to the binary string.
+ * @b: Pointer to the binary string.
  * Return: The converted unsigned integer.
  */
-unsigned int binary_to_uint(const char *y)
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int base = 1, result = 0, len = 0;
 
-	if (y == NULL)
+	if (b == NULL)
 		return (0);
 
-	while (y[len])
+	while (b[len])
 		len++;
 
 	while (len)
 	{
-		if (y[len - 1] != '0' && y[len - 1] != '1')
+		if (b[len - 1] != '0' && b[len - 1] != '1')
 			return (0);
 
-		if (y[len - 1] == '1')
+		if (b[len - 1] == '1')
 			result += base;
 		base *= 2;
 		len--;
